80845941c4de45ee4fa934cda1e40aa98223a6d5
Markus Mayer <widemeadows@gmail.com>
2019-10-05 17:51:59 +0200

    Add manual triggering of dissolve

---
+++ Assets/Scripts/DissolveTrigger.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class DissolveTrigger : MonoBehaviour
+{
+    private static readonly int DissolveTriggerPropertyId = Shader.PropertyToID("Vector1_195A31D6");
+
+    [SerializeField]
+    private SkinnedMeshRenderer meshRenderer;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            meshRenderer.material.SetFloat(DissolveTriggerPropertyId, Time.time);
+        }
+    }
+}
